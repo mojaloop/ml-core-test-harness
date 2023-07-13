0c7852d0be4d5a256eca504665056c81c52b0faa
Miguel de Barros <miguel@debarros.me>
2023-07-13 17:31:54 +0000

    updated k6/scipts/test.js to generate trastate headers, with util for traceParent header

--- docker/k6/scripts/test.js
+++ docker/k6/scripts/test.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
+import { check, sleep, crypto } from 'k6';
+// import { Trace } from './utils'
 
 export const options = {
   stages: [
@@ -17,16 +18,24 @@ console.log(`Env Vars -->
   ACCOUNT_LOOKUP_SERVICE_PORT=${__ENV.ACCOUNT_LOOKUP_SERVICE_PORT}
 `);
 
+const generateTrace = () => {
+  // Ref: https://github.com/mojaloop/mojaloop-specification/blob/master/fspiop-api/documents/Tracing%20v1.0.md#table-2--data-model-for-tracing-values
+  return `00-${crypto.randomBytes(16)}-${crypto.randomBytes(8)}-${crypto.randomBytes(1)}`
+}
+
 export default function () {
   const params = {
-      headers: {
-        'Accept': 'application/vnd.interoperability.parties+json;version=1.1',
-        'Content-Type': 'application/vnd.interoperability.parties+json;version=1.1',
-        'FSPIOP-Source': 'pinkbankfsp',
-        'Date': (new Date()).toUTCString()
-      },
-    };
-  const res = http.get(`http://${__ENV.ACCOUNT_LOOKUP_SERVICE_HOST}:${__ENV.ACCOUNT_LOOKUP_SERVICE_PORT}/parties/MSISDN/27713803912`, params);
+    headers: {
+      'Accept': 'application/vnd.interoperability.parties+json;version=1.1',
+      'Content-Type': 'application/vnd.interoperability.parties+json;version=1.1',
+      'FSPIOP-Source': 'perffsp1',
+      'Date': (new Date()).toUTCString(),
+      // 'traceParent': Trace.generateTraceParent(),
+      'traceState': `tx_end2end_start_ts=${Date.now()}`
+    },
+  };
+
+  const res = http.get(`http://${__ENV.ACCOUNT_LOOKUP_SERVICE_HOST}:${__ENV.ACCOUNT_LOOKUP_SERVICE_PORT}/parties/MSISDN/19012345002`, params);
   check(res, { 'status was 200': (r) => r.status == 200 });
   sleep(1);
 }
