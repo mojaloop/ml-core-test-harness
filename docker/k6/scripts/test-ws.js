55006adbf0579441e4b06ebe3db1defd184f0d08
Vijay <vijaya.guthi@infitx.com>
2023-07-19 14:41:51 +0000

    fix: issues with dashboards and ws test script

--- docker/k6/scripts/test-ws.js
+++ docker/k6/scripts/test-ws.js
@@ -2,15 +2,27 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 import crypto from "k6/crypto";
 import { WebSocket } from 'k6/experimental/websockets';
+// import { Trend, Rate } from 'k6/metrics';
+
+// const myTrend = new Trend('waiting_time', isTime=true);
+// const myTrend = new Trend('waiting_time', true);
+
+// const myRate = new Trend('rate_time');
 
 export const options = {
   stages: [
-    { duration: '15s', target: 1, vus: 1 },
+    { duration: '3m', target: 1, vus: 1 },
     // { duration: '1m30s', target: 10 },
     // { duration: '20s', target: 0 },
   ],
   tags: {
     testid: `${Date.now()}`,
+  },
+  thresholds: { // Ref: https://k6.io/docs/using-k6/workaround-to-calculate-iteration_duration/
+    'iteration_duration{scenario:default}': [`max>=0`],
+    'iteration_duration{group:::setup}': [`max>=0`],
+    'iteration_duration{group:::teardown}': [`max>=0`],
+    'http_req_duration{scenario:default}': [`max>=0`],
   },
 };
 
@@ -56,6 +68,7 @@ class TraceParent {
 }
 
 export default function () {
+  const startTs = Date.now();
 
   const payeeId = '19012345002';
   const traceParent = new TraceParent();
@@ -65,9 +78,13 @@ export default function () {
   const ws = new WebSocket(`ws://callback-handler-svc:3002/${wsChannel}`);
   
   ws.onmessage = (data) => {
+    // const deltaTs = Date.now() - startTs
+    // myTrend.add(deltaTs);
+    // myRate.add(deltaTs)
     console.log('a message received');
-    console.log(data);
+    // console.log(deltaTs);
     ws.close();
+    // sleep(1);
   };
 
   ws.onopen = () => {
@@ -78,13 +95,11 @@ export default function () {
         'FSPIOP-Source': 'perffsp1',
         'Date': (new Date()).toUTCString(),
         'traceparent': traceParent.toString(),
-        'tracestate': `tx_end2end_start_ts=${Date.now()}`
+        'tracestate': `tx_end2end_start_ts=${startTs}`
       },
     };
   
     const res = http.get(`http://${__ENV.ACCOUNT_LOOKUP_SERVICE_HOST}:${__ENV.ACCOUNT_LOOKUP_SERVICE_PORT}/parties/MSISDN/${payeeId}`, params);
     check(res, { 'http sync response status is 202': (r) => r.status == 202 });
   };
-
-  // sleep(1);
 }
