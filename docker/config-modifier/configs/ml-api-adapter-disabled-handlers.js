d9845188939c1be8e53a0c4dd5aa29944b10d6a3
Kevin Leyow <kleyow@gmail.com>
2023-08-02 14:27:12 -0500

    chore: split ml-api-adapter

---
+++ docker/config-modifier/configs/ml-api-adapter-disabled-handlers.js
@@ -0,0 +1,49 @@
+module.exports = {
+  "HOSTNAME": "http://ml-api-adapter",
+  "ENDPOINT_SOURCE_URL": "http://central-ledger:3001",
+  "ENDPOINT_HEALTH_URL": "http://central-ledger:3001/health",
+  "HANDLERS": {
+    "DISABLED": true,
+    "API": {
+      "DISABLED": false
+    }
+  },
+  "KAFKA": {
+    "CONSUMER": {
+      "NOTIFICATION": {
+        "EVENT": {
+          "config": {
+            "rdkafkaConf": {
+              "metadata.broker.list": "kafka:29092"
+            }
+          }
+        }
+      }
+    },
+    "PRODUCER": {
+      "TRANSFER": {
+        "PREPARE": {
+          "config": {
+            "rdkafkaConf": {
+              "metadata.broker.list": "kafka:29092"
+            }
+          }
+        },
+        "FULFIL": {
+          "config": {
+            "rdkafkaConf": {
+              "metadata.broker.list": "kafka:29092"
+            }
+          }
+        },
+        "GET": {
+          "config": {
+            "rdkafkaConf": {
+              "metadata.broker.list": "kafka:29092"
+            }
+          }
+        }
+      }
+    }
+  }
+}
