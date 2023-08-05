566eb78dc546cea71518d333eff0f3ada6ec4dac
Kevin Leyow <kleyow@gmail.com>
2023-08-05 03:59:13 +0000

    chore: add mysql cl dashboard

--- docker/sql-cl-init/01_permissions.sql
+++ docker/sql-cl-init/01_permissions.sql
@@ -2,7 +2,4 @@ ALTER USER 'central_ledger'@'%' identified WITH mysql_native_password by 'passwo
 CREATE USER 'exporter'@'%' IDENTIFIED BY 'password';
 GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'exporter'@'%';
 GRANT SELECT ON performance_schema.* TO 'exporter'@'%';
-CREATE USER 'grafanaReader' IDENTIFIED BY 'password';
-GRANT SELECT ON central_ledger.transferStateChange TO 'grafanaReader';
-GRANT SELECT ON information_schema.TABLES TO 'grafanaReader';
 FLUSH PRIVILEGES;
