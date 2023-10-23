4c649b25cf6c733ffde93a23ed2af81a9b873870
Kevin Leyow <kleyow@gmail.com>
2023-10-23 09:02:17 -0500

    chore: add scenario for complex quote testing (#40)

--- docker/config-modifier/configs/quoting-service-perf.js
+++ docker/config-modifier/configs/quoting-service-perf.js
@@ -4,8 +4,5 @@ module.exports = {
     "HOST": "mysql-cl"
   },
   "SIMPLE_ROUTING_MODE": false,
-  "SWITCH_ENDPOINT": "http://callback-handler-svc-cl-sim:3001/admin",
-  "CACHE": {
-    "EXPIRES_IN_MS": 60000
-  }
+  "SWITCH_ENDPOINT": "http://callback-handler-svc-cl-sim:3001/admin"
 }
