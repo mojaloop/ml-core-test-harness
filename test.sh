e051cb4f809a90f2d104c03aee40128107cd10ff
Kalin Krustev <kalin.krustev@gmail.com>
2024-04-19 12:13:09 +0000

    add discovery and e2e in test.sh

--- test.sh
+++ test.sh
@@ -3,6 +3,14 @@
 args="$1$2"
 
 case "$args" in
+discovery)
+    echo "Testing single account discovery"
+    env K6_SCRIPT_CONFIG_FILE_NAME=fspiopDiscoverySingle.json docker compose --project-name load -f docker-compose-load.yml up
+;;
+discoveries)
+    echo "Testing account discoveries"
+    env K6_SCRIPT_CONFIG_FILE_NAME=fspiopDiscovery.json docker compose --project-name load -f docker-compose-load.yml up -d
+;;
 quote)
     echo "Testing single quote"
     env K6_SCRIPT_CONFIG_FILE_NAME=fspiopSingleQuote.json docker compose --project-name load -f docker-compose-load.yml up
@@ -19,6 +27,14 @@ transfer)
     echo "Testing transfers"
     env K6_SCRIPT_CONFIG_FILE_NAME=fspiopTransfers.json docker compose --project-name load -f docker-compose-load.yml up -d
 ;;
+e2e)
+    echo "Testing multiple end to end"
+    env K6_SCRIPT_CONFIG_FILE_NAME=fspiopE2E.json docker compose --project-name load -f docker-compose-load.yml up -d
+;;
+e2esingle)
+    echo "Testing single end to end"
+    env K6_SCRIPT_CONFIG_FILE_NAME=fspiopE2Esingle.json docker compose --project-name load -f docker-compose-load.yml up
+;;
 simstart)
     echo "Starting the simulators"
     docker compose --project-name simulators -f docker-compose-perf.yml --profile 8dfsp --profile testing-toolkit --profile ttk-provisioning-remote-k8s --profile oracle up -d
@@ -34,10 +50,14 @@ simrestart)
 ;;
 *)
     echo "Usage: "
+    echo "   $0 discovery"
+    echo "   $0 discoveries"
     echo "   $0 quote"
     echo "   $0 quotes"
     echo "   $0 transfer"
     echo "   $0 transfers"
+    echo "   $0 e2e"
+    echo "   $0 e2e single"
     echo "   $0 sim start"
     echo "   $0 sim stop"
     echo "   $0 sim restart"
