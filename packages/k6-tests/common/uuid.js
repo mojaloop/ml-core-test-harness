e52528d053a1f65ade873627bd35c37a88033992
Kalin Krustev <kalin.krustev@gmail.com>
2024-12-17 09:50:17 +0200

    feat: iso 20022 support (#86)

--- packages/k6-tests/common/uuid.js
+++ packages/k6-tests/common/uuid.js
@@ -9,3 +9,6 @@ export function uuid() {
   return `${t.substring(0,8)}-${t.substring(8,12)}-4${random(3)}-9${random(3)}-${random(12)}`
 }
 
+export function ulid() {
+  return `${Date.now().toString(16).padStart(12, 0)}${random(14)}`.toUpperCase()
+}
