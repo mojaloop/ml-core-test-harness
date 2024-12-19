e5a5142ef2cba8b088a93cc7e50a6b4022c0460e
Kalin Krustev <kalin.krustev@gmail.com>
2024-12-19 13:46:26 +0200

    feat: start building docker image with the k6 scripts (#90)

--- packages/k6-tests/common/replaceHeaders.js
+++ packages/k6-tests/common/replaceHeaders.js
@@ -1,7 +1,7 @@
 export function replaceHeaders(headers) {
     if (__ENV.API_TYPE === 'iso20022') {
         headers['Accept'] = headers['Accept'].replace('.interoperability.', '.interoperability.iso20022.').replace(/version=.*/, 'version=2.0');
-        headers['Content-Type'] = headers['Accept'].replace('.interoperability.', '.interoperability.iso20022.').replace(/version=.*/, 'version=2.0');
+        headers['Content-Type'] = headers['Content-Type'].replace('.interoperability.', '.interoperability.iso20022.').replace(/version=.*/, 'version=2.0');
     }
     return headers;
 }
