de1a7b3acf9045fb9faa28ed99cd40b15df5b631
vijayg10 <33152110+vijayg10@users.noreply.github.com>
2025-05-22 21:41:15 +0530

    fix: perf stack issues (#108)
    * fix: perf stack issues
    
    * feat: added getTransfers
    
    * chore: version

--- docker/config-modifier/configs/central-ledger-disabled-handlers.js
+++ docker/config-modifier/configs/central-ledger-disabled-handlers.js
@@ -15,6 +15,16 @@ module.exports = {
     "PASSWORD": "",
     "DATABASE": "mlos"
   },
+  "PROXY_CACHE": {
+    "enabled": true,
+    "type": "redis",
+    "proxyConfig": {
+      // We need to unset cluster as there is an issue in proxy lib. The above type parameter is not being considered.
+      "cluster": undefined,
+      "host": "redis",
+      "port": 6379,
+    }
+  },
   "HANDLERS": {
     "DISABLED": true,
     "API": {
