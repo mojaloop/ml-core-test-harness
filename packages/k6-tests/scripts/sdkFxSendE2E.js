f2ddfa0b62a60220bd3c0dd7964e717d4b1927f8
vijayg10 <33152110+vijayg10@users.noreply.github.com>
2025-06-16 14:03:24 +0530

    chore: added sdk metrics (#115)
    * chore: added sdk metrics
    
    * chore: parameterize some variables

--- packages/k6-tests/scripts/sdkFxSendE2E.js
+++ packages/k6-tests/scripts/sdkFxSendE2E.js
@@ -5,17 +5,12 @@ import { getTwoItemsFromArray } from "../common/utils.js";
 
 function log() {
   console.log('Env Vars -->');
-  console.log(`  K6_SCRIPT_FSPIOP_FSP_POOL=${__ENV.K6_SCRIPT_FSPIOP_FSP_POOL}`);
+  console.log(`  K6_SCRIPT_SDK_FSP_POOL=${__ENV.K6_SCRIPT_SDK_FSP_POOL}`);
   console.log(`  K6_SCRIPT_ABORT_ON_ERROR=${__ENV.K6_SCRIPT_ABORT_ON_ERROR}`);
-  console.log(`  K6_SCRIPT_SDK_ENDPOINT_URL=${__ENV.K6_SCRIPT_SDK_ENDPOINT_URL}`);
 }
 
 const fspList = JSON.parse(__ENV.K6_SCRIPT_SDK_FSP_POOL || '[]');
 const idType = __ENV.K6_SCRIPT_ID_TYPE || 'ACCOUNT_ID';
-const amount = __ENV.K6_SCRIPT_FX_E2E_SOURCE_AMOUNT?.toString() || '2'
-const currency = __ENV.K6_SCRIPT_FX_E2E_SOURCE_CURRENCY
-// const targetAmount = __ENV.K6_SCRIPT_FX_E2E_TARGET_AMOUNT?.toString() || '2'
-// const targetCurrency = __ENV.K6_SCRIPT_FX_E2E_TARGET_CURRENCY
 
 const abortOnError = (__ENV.K6_SCRIPT_ABORT_ON_ERROR && __ENV.K6_SCRIPT_ABORT_ON_ERROR.toLowerCase() === 'true') ? true : false
 
@@ -38,6 +33,8 @@ export function sdkFxSendE2E() {
     const payeeFspId = payeeFsp['fspId'];
     const payerPartyId = payerFsp['partyId'];
     const payeePartyId = payeeFsp['partyId'];
+    const amount = payerFsp['amount'] || '2';
+    const currency = payerFsp['currency'] || 'XXX';
 
     const params = {
       tags: {
@@ -49,6 +46,9 @@ export function sdkFxSendE2E() {
         'Content-Type': 'application/json',
       },
     };
+
+    // Use payerFsp.outboundUrl instead of __ENV.K6_SCRIPT_SDK_ENDPOINT_URL
+    const sdkEndpointUrl = payerFsp['outboundUrl'];
 
     const body = {
       "homeTransactionId": "string",
@@ -73,14 +73,14 @@ export function sdkFxSendE2E() {
     }
 
     // Lets send the FSPIOP POST /transfers request
-    const postTransferResponse = http.post(`${__ENV.K6_SCRIPT_SDK_ENDPOINT_URL}/transfers`, JSON.stringify(body), params);
+    const postTransferResponse = http.post(`${sdkEndpointUrl}/transfers`, JSON.stringify(body), params);
     console.log('postTransferResponse', postTransferResponse)
     check(postTransferResponse, { 'TRANSFERS__POST_TRANSFERS_RESPONSE_IS_200' : (r) => r.status == 200 });
 
     const transferId = JSON.parse(postTransferResponse.body).transferId
 
     if (postTransferResponse.status == 200) {
-      const putTransferacceptPartyResponse = http.put(`${__ENV.K6_SCRIPT_SDK_ENDPOINT_URL}/transfers/${transferId}`, JSON.stringify({
+      const putTransferacceptPartyResponse = http.put(`${sdkEndpointUrl}/transfers/${transferId}`, JSON.stringify({
         "acceptParty": true
       }), params);
       console.log('putTransferacceptPartyResponse', putTransferacceptPartyResponse)
@@ -88,14 +88,14 @@ export function sdkFxSendE2E() {
 
       if (putTransferacceptPartyResponse.status == 200) {
         // Call acceptConversion before acceptQuote
-        const putTransferAcceptConversionResponse = http.put(`${__ENV.K6_SCRIPT_SDK_ENDPOINT_URL}/transfers/${transferId}`, JSON.stringify({
+        const putTransferAcceptConversionResponse = http.put(`${sdkEndpointUrl}/transfers/${transferId}`, JSON.stringify({
           "acceptConversion": true
         }), params);
         console.log('putTransferAcceptConversionResponse', putTransferAcceptConversionResponse)
         check(putTransferAcceptConversionResponse, { 'TRANSFERS__PUT_TRANSFERS_ACCEPT_CONVERSION_RESPONSE_IS_200' : (r) => r.status == 200 });
 
         if (putTransferAcceptConversionResponse.status == 200) {
-          const putTransferAcceptQuoteResponse = http.put(`${__ENV.K6_SCRIPT_SDK_ENDPOINT_URL}/transfers/${transferId}`, JSON.stringify({
+          const putTransferAcceptQuoteResponse = http.put(`${sdkEndpointUrl}/transfers/${transferId}`, JSON.stringify({
             "acceptQuote": true
           }), params);
           console.log('putTransferAcceptQuoteResponse', putTransferAcceptQuoteResponse)
