e7f9c8640f18fb000b791b4d17895c32539a2b70
senkyte <KOH_ZIHAO_ALEXANDER@S2021.SSTS.EDU.SG>
2026-08-22 00:35:17 +0800

    fix: added license, gen ai declaration

--- get_mojaloop_versions.sh
+++ get_mojaloop_versions.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
+: <<'COMMENT'
+License
+ --------------
+ Copyright © 2026 Mojaloop Foundation
 
+ The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0
+ (the "License") and you may not use these files except in compliance with the [License](http://www.apache.org/licenses/LICENSE-2.0).
+
+ You may obtain a copy of the License at [http://www.apache.org/licenses/LICENSE-2.0](http://www.apache.org/licenses/LICENSE-2.0)
+
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the [License](http://www.apache.org/licenses/LICENSE-2.0).
+
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Mojaloop Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+
+ * Mojaloop Foundation
+- Alexander Koh <senkoh.san@gmail.com> [Assisted by GPT-5.6 Luna] 
+
+ --------------
+COMMENT
 set -e
 
 # Gets Mojaloop version variables from .env
